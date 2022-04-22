cecbe6a2e900536696e3dd2bae3200089d668dc0
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-04-22 15:28:09 +0300

    Added a text to the file

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,7 +14,14 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-
+          children: [
+            Row(
+            children: [
+              Text("wapi", style: GoogleFonts.roboto(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),),
+              Text("Registration", style: GoogleFonts.roboto(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),),
+            ],
+          )
+          ],
         )
       )
     );
