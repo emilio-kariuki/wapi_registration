34d924ab475ad33c57c944cd3e3e2acbc38b5f73
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-04-22 21:01:43 +0300

    added a button and Text

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,21 +11,136 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<bool> isSelected = [true, false];
+
+  @override
+  void initState() {
+    // this is for 3 buttons, add "false" same as the number of buttons here
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-            children: [
-              Text("wapi", style: GoogleFonts.roboto(fontSize: 18, fontWeight: FontWeight.normal, color: Colors.black),),
-              Text("Registration", style: GoogleFonts.roboto(fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),),
+        body: SingleChildScrollView(
+            child: SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "wapi",
+                  style: GoogleFonts.roboto(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black),
+                ),
+                Text(
+                  "Registration",
+                  style: GoogleFonts.roboto(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              ],
+            ),
+          ),
+          const Action(action: "Fetch"),
+          Button(size: size, actionString: "Fetch", action: () {}),
+          const Action(action: "Bluetooth"),
+          ToggleButtons(
+            children: const [
+              Icon(Icons.bluetooth_connected),
+              Icon(Icons.bluetooth_disabled)
             ],
-          )
-          ],
-        )
-      )
+            isSelected: isSelected,
+            onPressed: (int index) {
+              setState(() {
+                for (int index = 0; index < isSelected.length; index++) {
+                  if (index == index) {
+                    isSelected[index] = true;
+                  } else {
+                    isSelected[index] = false;
+                  }
+                }
+              });
+            },
+            color: Colors.green,
+            disabledColor: Colors.green,
+            selectedColor: const Color.fromARGB(255, 4, 67, 119),
+            borderRadius: BorderRadius.circular(30),
+            renderBorder: false,
+          ),
+        ],
+      ),
+    )));
+  }
+}
+
+class Button extends StatelessWidget {
+  final Function() action;
+  final String actionString;
+  const Button({
+    Key? key,
+    required this.size,
+    required this.action,
+    required this.actionString,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15, bottom: 10),
+      child: SizedBox(
+        height: size.height * 0.06,
+        width: size.width * 0.55,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(
+                const Color.fromARGB(255, 14, 14, 20)),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+                side: const BorderSide(
+                  color: Color.fromARGB(255, 14, 14, 20),
+                  width: 2.0,
+                ),
+              ),
+            ),
+          ),
+          child: Text(actionString, style: GoogleFonts.roboto(fontSize: 20)),
+          onPressed: () => exit(0),
+        ),
+      ),
+    );
+  }
+}
+
+class Action extends StatelessWidget {
+  final String action;
+  const Action({
+    Key? key,
+    required this.action,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, top: 10),
+      child: Row(children: [
+        Text(
+          action,
+          style: GoogleFonts.roboto(
+              fontSize: 20, fontWeight: FontWeight.w500, color: Colors.black),
+        ),
+      ]),
     );
   }
 }
