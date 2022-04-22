3a2e0c74b0c713e248f0191fac1b23b86b620390
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-04-22 15:00:35 +0300

    correct the error in the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:wapi/screens/Home.dart';
 
 void main() {
   runApp(const MaterialApp(
   home: Home(),
+  debugShowCheckedModeBanner: false
   ));
 }
 
