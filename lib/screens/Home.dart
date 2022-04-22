3e05d029bb76b593b545f20ebd69aed9d50f8fef
Emilio Kariuki <emilio113kariuki@gmail.com>
2022-04-23 01:25:12 +0300

    added the bluetooth files

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,12 +1,15 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
-import 'package:custom_switch/custom_switch.dart';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:wapi/screens/entry.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  final bool start;
+  const Home({Key? key, this.start = true}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
@@ -16,65 +19,137 @@ class _HomeState extends State<Home> {
   List<bool> isSelected = [true, false];
   bool status = false;
 
-  void bluetoothConnect() async{
-    try {
-      BluetoothConnection connection = await BluetoothConnection.toAddress(address);
-    } catch (e) {
-      Fluttertoast.showToast(
-        msg: "$e",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.Bottom,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0
-    );
-    }
-  }
+  late StreamSubscription<BluetoothDiscoveryResult> _streamSubscription;
+  List<BluetoothDiscoveryResult> results = [];
+  late bool isDiscovering;
 
   @override
   void initState() {
-    // this is for 3 buttons, add "false" same as the number of buttons here
-
     super.initState();
+
+    isDiscovering = widget.start;
+    if (isDiscovering) {
+      _startDiscovery();
+    }
+  }
+
+  void _restartDiscovery() {
+    setState(() {
+      results.clear();
+      isDiscovering = true;
+    });
+
+    _startDiscovery();
+  }
+
+  void _startDiscovery() {
+    _streamSubscription =
+        FlutterBluetoothSerial.instance.startDiscovery().listen((r) {
+      setState(() {
+        results.add(r);
+      });
+    });
+
+    _streamSubscription.onDone(() {
+      setState(() {
+        isDiscovering = false;
+      });
+    });
+  }
+
+  // @TODO . One day there should be `_pairDevice` on long tap on something... ;)
+
+  @override
+  void dispose() {
+    // Avoid memory leak (`setState` after dispose) and cancel discovery
+    _streamSubscription.cancel();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
+        appBar: AppBar(
+          actions: <Widget>[
+            isDiscovering
+                ? FittedBox(
+                    child: Container(
+                      margin: const EdgeInsets.all(16.0),
+                      child: const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  )
+                : IconButton(
+                    icon: const Icon(Icons.replay),
+                    onPressed: _restartDiscovery,
+                  )
+          ],
+        ),
         body: SingleChildScrollView(
             child: SafeArea(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "wapi",
-                  style: GoogleFonts.roboto(
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.black),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "wapi",
+                      style: GoogleFonts.roboto(
+                          fontSize: 20,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black),
+                    ),
+                    Text(
+                      "Registration",
+                      style: GoogleFonts.roboto(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  ],
                 ),
-                Text(
-                  "Registration",
-                  style: GoogleFonts.roboto(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
+              ),
+              const Action(action: "Fetch"),
+              Button(size: size, actionString: "Fetch", action: () {}),
+              const Action(action: "Bluetooth"),
+              Row(
+                children: [
+                  isDiscovering
+                      ? const Text("Discovering...")
+                      : const Text("Discovered Devices")
+                ],
+              ),
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: ListView.builder(
+                        itemCount: results.length,
+                        itemBuilder: (BuildContext context, index) {
+                          BluetoothDiscoveryResult result = results[index];
+                          return BluetoothDeviceListEntry(
+                            device: result.device,
+                            rssi: result.rssi,
+                            onTap: () {
+                              Navigator.of(context).pop(result.device);
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          const Action(action: "Fetch"),
-          Button(size: size, actionString: "Fetch", action: () {}),
-          const Action(action: "Bluetooth"),
-        ],
-      ),
-    )));
+        )));
   }
 }
 
